f3e716e6f05581a7aa68dd20219a27d922441aba
Sergio Hernández Jerez <sergiohernandezjerez@gmail.com>
2021-10-04 18:40:06 +0200

    Implement Service class
    Implementación de la clase Service para acceder a los DAO sin utilizar el controlador

---
+++ src/main/java/com/thefundidors/springboot/app/models/service/IClienteService.java
@@ -0,0 +1,16 @@
+package com.thefundidors.springboot.app.models.service;
+
+import java.util.List;
+
+import com.thefundidors.springboot.app.models.entity.Cliente;
+
+public interface IClienteService {
+
+	public List<Cliente> findAll();
+
+	public void save(Cliente cliente);
+
+	public Cliente findOne(Long id);
+
+	public void delete(Long id);
+}
