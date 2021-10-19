4d2286844fee77a4c5316bae9c0d642cbd22f1bf
Sergio Hernández Jerez <sergiohernandezjerez@gmail.com>
2021-10-19 18:22:09 +0200

    Creados flashmessages para info y errores

--- src/main/java/com/thefundidors/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/thefundidors/springboot/app/controllers/ClienteController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.thefundidors.springboot.app.models.dao.IClienteDao;
 import com.thefundidors.springboot.app.models.entity.Cliente;
@@ -25,51 +26,60 @@ public class ClienteController {
 	@Autowired
 	private IClienteService clienteService;
 
-	@RequestMapping(value="/listar", method = RequestMethod.GET)
+	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public String listar(Model model) {
 		model.addAttribute("titulo", "Listado de clientes");
 		model.addAttribute("clientes", clienteService.findAll());
 		return "listar";
 	}
-	
-	@RequestMapping(value="/form")
+
+	@RequestMapping(value = "/form")
 	public String crear(Map<String, Object> model) {
 		model.put("titulo", "Formulario de Cliente");
 		Cliente cliente = new Cliente();
 		model.put("cliente", cliente);
 		return "form";
 	}
-	
-	@RequestMapping(value="/form", method=RequestMethod.POST)
-	public String guarda(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
-		if(result.hasErrors()) {
+
+	@RequestMapping(value = "/form", method = RequestMethod.POST)
+	public String guarda(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash,
+			SessionStatus status) {
+		if (result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario del cliente");
 			return "form";
 		}
+		String mensajeFlash = (cliente.getId() != null) ? "Cliente editado con éxito" : "Cliente creado con éxito";
 		clienteService.save(cliente);
 		status.setComplete();
+		flash.addFlashAttribute("success", mensajeFlash);
 		return "redirect:listar";
 	}
-	
-	@RequestMapping(value="/form/{id}")
-	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
+
+	@RequestMapping(value = "/form/{id}")
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 		Cliente cliente = null;
-		if(id > 0) {
+		if (id > 0) {
 			cliente = clienteService.findOne(id);
-		}else {
+			if (cliente == null) {
+				flash.addFlashAttribute("error", "El ID del cliente no existe en la BBDD");
+				return "redirect:/listar";
+			}
+		} else {
+			flash.addFlashAttribute("error", "El ID del cliente no puede ser 0");
 			return "redirect:/listar";
 		}
 		model.put("cliente", cliente);
 		model.put("titulo", "Editar Cliente");
 		return "form";
 	}
-	
-	@RequestMapping(value="/eliminar/{id}")
-	public String eliminar(@PathVariable(value="id") Long id) {
-		if(id > 0) {
+
+	@RequestMapping(value = "/eliminar/{id}")
+	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
+		if (id > 0) {
 			clienteService.delete(id);
+			flash.addFlashAttribute("success", "Cliente eliminado con éxito");
 		}
-		
+
 		return "redirect:/listar";
 	}
 
