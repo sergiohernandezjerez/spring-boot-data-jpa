0b8b2d5861fb3a2b88a2ded183d19e55e5561ec0
Sergio Hernández Jerez <sergiohernandezjerez@gmail.com>
2021-10-19 17:11:07 +0200

    Utilizafoms fragments para reutilizar el codigo que se repite en las dos plantillas: listar y form

--- src/main/java/com/thefundidors/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/thefundidors/springboot/app/controllers/ClienteController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
